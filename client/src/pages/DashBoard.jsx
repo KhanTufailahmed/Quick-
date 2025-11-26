602d68220dc1be07b42d569503f098aa412b1a62
Khan Tufailahmed <tufailahmedkhan708@gmail.com>
2025-11-26 23:10:30 +0530

    Progress done

--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -1,10 +1,58 @@
-import React from 'react'
+import { dummyCreationData } from "@/assets/assets";
+import CreationItem from "@/components/Dashboard/CreationItem";
+import { Protect } from "@clerk/clerk-react";
+import { Gem, Sparkles } from "lucide-react";
+import React, { use, useEffect, useState } from "react";
 const DashBoard = () => {
+  const [creations, setCreations] = useState([]);
+  const getDeshboardData = async () => {
+    setCreations(dummyCreationData);
+  };
+
+  useEffect(() => {
+    getDeshboardData();
+  }, []);
   return (
-    <div>
-      <h1>DashBoard DashBoard</h1>
+    <div className="h-full overflow-y-scroll p-6">
+      <div className="flex justify-start gap-4 flex-wrap">
+        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Total Creations</p>
+            <h2 className="text-xl font-semibold">{creations.length}</h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-b from-[#3588F2] to-[#0BB0D7] text-white flex items-center justify-center">
+            <Sparkles className="w-5 text-white"></Sparkles>
+          </div>
+        </div>
+
+        <div className="flex justify-between items-center w-72 p-4 px-6 bg-white rounded-xl border border-gray-200">
+          <div className="text-slate-600">
+            <p className="text-sm">Active plan</p>
+            <h2
+              className="text-xl font-semibold"
+              plan="premium"
+              fallback="free"
+            >
+              <Protect>Premium</Protect>
+            </h2>
+          </div>
+          <div className="w-10 h-10 rounded-lg bg-gradient-to-b from-[#FF61C5] to-[#9E53EE] text-white flex items-center justify-center">
+            <Gem className="w-5 text-white"></Gem>
+          </div>
+        </div>
+      </div>
+
+      <div className="space-y-3">
+        <p className="mt-6 mb-4">Recent Creations</p>
+        {/* <CreationItem /> */}
+        {
+          creations.map((item,index)=>(
+            <CreationItem key={index} item={item}></CreationItem>
+          ))
+        }
+      </div>
     </div>
   );
-}
+};
 
-export default DashBoard
+export default DashBoard;
