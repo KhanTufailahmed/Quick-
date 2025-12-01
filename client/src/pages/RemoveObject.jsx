ed92434666cf16b2f7e126e64fabffca6794988c
Khan Tufailahmed <tufailahmedkhan708@gmail.com>
2025-12-02 01:03:03 +0530

    4th

--- client/src/pages/RemoveObject.jsx
+++ client/src/pages/RemoveObject.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { Eraser, Scissors, Sparkles, SquareScissors } from 'lucide-react';
+import React, { useState } from 'react'
 
 const RemoveObject = () => {
+  const [input, setInput] = useState("");
+  const [object, setObject] = useState("");
+  
+    const onSubmitHandler = (e) => {
+      e.preventDefault();
+      console.log(input, object);
+    };
   return (
-    <div>
-      <h1>RemoveObject RemoveObject</h1>
+    <div className="h-full overflow-y-scroll p-6 flex items-start flex-wrap gap-4 text-slate-700">
+      <form
+        onSubmit={onSubmitHandler}
+        className="w-full max-w-lg p-4 bg-white rounded-lg border border-gray-200"
+      >
+        <div className="flex items-center gap-3">
+          <Sparkles className="w-6 text-[#4A7AFF]"></Sparkles>
+          <h1 className="text-xl font-semibold">Object Removal</h1>
+        </div>
+        <p className="mt-6 text-sm font-medium">Upload Image</p>
+        <input
+          type="file"
+          onChange={(e) => setInput(e.target.files[0])}
+          accept="image/*"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300 text-gray-600"
+          required
+        />
+        <p className="mt-6 text-sm font-medium">Describe object to remove</p>
+
+        <textarea
+          rows={4}
+          value={object}
+          onChange={(e)=>setObject(e.target.value)}
+          placeholder="e.g., car in background, tree from the image"
+          className="w-full p-2 px-3 mt-2 outline-none text-sm rounded-md border border-gray-300"
+          required
+        />
+        <p className="text-xs text-gray-500 font-light mt-1">
+          Be specific about what you want to remove
+        </p>
+
+        <button className="w-full flex justify-center items-center gap-2 bg-gradient-to-r from-[#417DF6] to-[#8E37EB] text-white px-4 py-2 mt-6 text-sm rounded-lg cursor-pointer">
+          <Scissors className="w-5"></Scissors>
+          Remove object
+        </button>
+      </form>
+      <div className="w-full max-w-lg p-4 bg-white rounded-lg flex flex-col border border-gray-200 min-h-96 max-h-[600px]">
+        <div className="flex item-centre gap-3">
+          <Scissors className="w-6 h-6 text-[#437AF5]"></Scissors>
+          <h1 className="text-xl font-semibold">Processed Image</h1>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <div className="text-sm flex flex-col items-center gap-5 text-gray-400">
+            <Scissors className="w-9 h-9"></Scissors>
+            <p>Upload an image and describe what to remove</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
