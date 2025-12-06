622a4a483c691bad225d01281a84e46b010b0d14
Khan Tufailahmed <tufailahmedkhan708@gmail.com>
2025-12-07 00:37:00 +0530

    created the middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,28 @@
+//middleware to verify userId and has Premium Plan
+
+import { clerkClient } from "@clerk/express";
+import { use } from "react";
+
+export const auth = async (req, res, next) => {
+  try {
+    const { userId, has } = await req.auth();
+    const hasPremiumPlan = await has({ plan: "premium" });
+
+    const user = await clerkClient.users.getUser(userId);
+
+    if (!hasPremiumPlan && user.privateMetadata.free_usage) {
+      req.free_usage = user.privateMetadata.free_usage;
+    } else {
+      await clerkClient.users.updateUserMetadata(userId, {
+        privateMetadata: { free_usage: 0 },
+      });
+
+      req.free_usage = 0;
+    }
+
+    req.plan = hasPremiumPlan ? "premium" : "free";
+    next();
+  } catch (error) {
+    return res.status(400).json({ message: error.message, succcess: false });
+  }
+};
