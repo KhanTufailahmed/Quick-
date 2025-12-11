0493f6aab0d44e88ce18c83887e8f63ebd44f38e
Khan Tufailahmed <tufailahmedkhan708@gmail.com>
2025-12-12 01:31:58 +0530

    fixed

--- server/server.js
+++ server/server.js
@@ -23,3 +23,7 @@ app.use(requireAuth());
 app.use("/api/ai", router);
 app.use("/api/user", userRouter );
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
